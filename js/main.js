d8bf630ae44def6f38264efa1a0f236424279c70
Timothy Prestby <tjprestby@gmail.com>
2019-03-28 21:45:53 -0500

    Began chart

--- js/main.js
+++ js/main.js
@@ -2,13 +2,14 @@
 (function(){ 
 //Pseudo global variables 
 var attrArray = ['Passenger Traffic of Highways(10000 persons)','Freight Traffic of Highways(10000 tons)','Average Wage of Employed Persons in Urban Units(US dollars)','Total Value of Technical Market(100 million US dollars)',"Total Value of Imports of Foreign-funded Enterprises(1,000 US dollars)","Total Value of Exports of Foreign-funded Enterprises(1,000 US dollars)",'Rate of Loss Due to Bad Quality of Manufactured Products(%)'];
+var csvData;
 //Set initial attribute
 var expressed = attrArray[0]
 
 //Function: set up choropleth map
 function setMap(){
     //Assign map frame dimensions
-    var width = 560,
+    var width = window.innerWidth * 0.5,
         height = 460;
 
     //Create new SVG container for the map 
@@ -20,10 +21,10 @@ function setMap(){
     
     //Create Albers equal area conic projection centered on China
     var projection = d3.geoAlbers()
-        .center([105.00, 45.60])
+        .center([115.00, 20.60])
         .rotate([-7.18, 43.64, 0])
         .parallels([3, 45.94])
-        .scale(1500.28)
+        .scale(500)
         .translate([width / 2, height / 2]);
 
     //Create path generator
@@ -48,7 +49,6 @@ function setMap(){
         csvData = data[0];
         asia = data[1];
         china = data[2];
-        console.log(csvData)
         //Translate TopoJSON into JSON
         var asiaCountries = topojson.feature(asia, asia.objects.asia),
             chinaRegions = topojson.feature(china, china.objects.provinces).features;
@@ -73,7 +73,7 @@ function setMap(){
 };
 
 //Function: set enumeration units//
-function setEnumerationUnits(chinaRegions, map, path){
+function setEnumerationUnits(chinaRegions, map, path, colorScale){
     //Add China provinces to map 
     var regions = map.selectAll('.regions')
     //Add data ass array
@@ -86,7 +86,10 @@ function setEnumerationUnits(chinaRegions, map, path){
     .attr('class', function(d){
         return 'regions ' + d.properties.name;
     })
-    .attr('d', path);
+    .attr('d', path)
+    .style('fill', function (d){
+        return choropleth(d.properties, colorScale);
+    });
 };
 
 //Function: set graticule//
@@ -165,6 +168,64 @@ function makeColorScale(data){
     colorScale.domain(domainArray);
     return colorScale;
 };
+
+//Function: test for data value and return color//
+function choropleth(props, colorScale){
+    //Make sure attribute value is a number
+    var val = parseFloat(props[expressed]);
+    //If attribute value exists, assign a color; otherwise assign gray
+    if (typeof val == 'number' && !isNaN(val)){
+        return colorScale(val);
+    } else {
+        return "#CCC";
+    };
+};
+
+//Function: create bubble chart//
+function createBubble(csvData, colorScale){
+    var diameter = 500;
+    var bubble = d3.pack()
+        .size([diameter, diameter])
+        .padding(1.5);
+
+    var svg = d3.select("body")
+        .append("svg")
+        .attr("width",  window.innerWidth * 0.425)
+        .attr("height", 460)
+        .attr("class", "bubble");
+    //Process CSV data
+
+        //bubbles needs very specific format, convert data to this.
+        var nodes = bubble.nodes({children:csvData}).filter(function(d) { return !d.children; });
+    
+        //setup the chart
+        var bubbles = svg.append("g")
+            .attr("transform", "translate(0,0)")
+            .selectAll(".bubble")
+            .data(nodes)
+            .enter();
+    
+        //create the bubbles
+        bubbles.append("circle")
+            .attr("r", function(d){ return d.r; })
+            .attr("cx", function(d){ return d.x; })
+            .attr("cy", function(d){ return d.y; })
+            .style("fill", function(d) { return color(d.value); });
+    
+        //format the text for each bubble
+        bubbles.append("text")
+            .attr("x", function(d){ return d.x; })
+            .attr("y", function(d){ return d.y + 5; })
+            .attr("text-anchor", "middle")
+            .text(function(d){ return d["Fruit"]; })
+            .style({
+                "fill":"white", 
+                "font-family":"Helvetica Neue, Helvetica, Arial, san-serif",
+                "font-size": "12px"
+            });
+    };
+ 
+createBubble();
 
 window.onload = setMap();
 
